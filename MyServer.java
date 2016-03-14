6f7712f3727770041392d24f94315d6abab3bfe3
mohdjahid <mohd.jahid.jmi@gmail.com>
2016-03-14 09:53:10 +0530

    Fully Functioning Chat Server in Java
    It send massages within the group and images as well.

--- MyServer.java
+++ MyServer.java
@@ -2,12 +2,19 @@ import java.io.*;
 import java.net.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.Graphics2D;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 
 public class MyServer extends JFrame implements ActionListener, Runnable
 {
 	JLabel l;
 	JTextArea sendtxt,recievetxt;
 	JButton send;
+
+	BufferedImage readImage;
 
 	MyServer(String s)
 	{
@@ -28,7 +35,9 @@ public class MyServer extends JFrame implements ActionListener, Runnable
 		setVisible(true);
 	}
 
-	static int i=0;
+	static int i=0;  //USERS ARRAY
+	static boolean[] onlineUser=new boolean[5];
+	static String[] username=new String[5]; 
 	Socket[] s=new Socket[5];
 	BufferedReader[] br=new BufferedReader[6];
 	PrintStream[] ps=new PrintStream[6];
@@ -41,19 +50,19 @@ public class MyServer extends JFrame implements ActionListener, Runnable
 		{	
 			try
 			{	
-					Thread t=new Thread(obj);
 					ServerSocket ss=new ServerSocket(9786);
 					System.out.println("waiting for request...");
-					obj.connectedUsers();
 					obj.s[i]=ss.accept();
+					onlineUser[i]=true;
+					Thread t=new Thread(obj);
 					i++; 
 					System.out.println(obj.s[i-1]);				
 					t.start();
-					System.out.println("YEAH");	
+					System.out.println("YEAH");
 			}
 			catch (Exception e)
 			{
-				//System.out.println("Not Found data for Socket: "+e);
+				// System.out.println("Not Found data for Socket: "+e);
 			}	
 		}
 	}
@@ -80,31 +89,71 @@ public class MyServer extends JFrame implements ActionListener, Runnable
 	   final int count=i-1;
 	   try {	
 			System.out.println("Request accepted");
+			
 			br[i-1]=new BufferedReader(new InputStreamReader(s[i-1].getInputStream()));
 			ps[i-1]=new PrintStream(s[i-1].getOutputStream());
+			username[count]=br[count].readLine();
+			connectedUsers();
 			while(true)
 			  {
 				String str=br[count].readLine();
-				for(int j=0;j<i;j++)	
-					{		
-				    		ps[j].println("Client "+(count+1)+" : "+str);
-					 		ps[j].flush();					 	
-					}
-
-				if(str.equals("exit")==true)
+                 
+                if(str.equals("image")==true) 
+				{
+					BufferedImage image = ImageIO.read(s[count].getInputStream());
+					for(int j=0;j<i;j++)	
 					{
-						System.out.print("Connection lost...");
-						System.exit(1);
-					}	
-				recievetxt.setText(recievetxt.getText()+str+"\n");
+        	    			 ps[j].println("image");
+	        	    		 ps[j].flush();
+	        	    		 ImageIO.write(image,"png",s[j].getOutputStream());
+					}
+				} 
+				else if(str.equals("exit")==true)
+					{
+							System.out.print("Connection lost...");
+							System.exit(1);
+					}
+						
+					else
+					{
+						for(int j=0;j<i;j++)	
+						{		
+						   		ps[j].println(username[count]+" : "+str);
+						 		ps[j].flush();					 	
+						}
+						recievetxt.setText(recievetxt.getText()+str+"\n");
+					} 
 			  }
 			}
-			catch(Exception e){e.printStackTrace();}
+			catch(Exception e)
+			{
+				e.printStackTrace(); 
+				onlineUser[count]=false;
+				connectedUsers();
+			}
 	}
 
 	public void connectedUsers()
 	{
 		for(int j=0;j<i;j++)
-			System.out.println("Client "+(j+1)+" : "+s[j].isConnected());
+		{
+			System.out.println("Client "+(j+1)+"  "+username[j]+" "+onlineUser[j]);
+		}
+		
+		for(int j=0;j<i;j++)
+		{
+			ps[j].println("onlineuser");
+		    ps[j].flush();
+			for(int k=0;k<i;k++)
+			{
+ 				if(onlineUser[k])
+ 				{	
+ 					ps[j].println(username[k]);
+			    	ps[j].flush();
+			    }
+			}
+		    ps[j].println("endusers");
+		    ps[j].flush();
+		}
 	}
 }
